898fcd66525dbe5182b2ce4bcbdd9e79b38c4a8e
Dmitriy Fedotov <dmitriyfedotov@Dmitriys-MacBook-Pro.local>
2022-06-26 19:10:48 +0300

    added all sales to telegram message

--- parser/telegram_messages.py
+++ parser/telegram_messages.py
@@ -1,13 +1,22 @@
 import telebot
 
+from mvideo_parser import MvideoParser
 from settings import TELEGRAM_TOKEN
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
 
+parser = MvideoParser()
 
-@bot.message_handler(commands=['start', 'help'])
+
+def get_all_sales():
+    return parser.get_all_discounts_today()
+
+
+@bot.message_handler(commands=['sales'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    tv_on_sale = get_all_sales()
+    for tv in tv_on_sale:
+        bot.reply_to(message, tv)
 
 
 bot.infinity_polling()
