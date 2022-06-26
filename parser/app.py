a06701f8f891a0c442905957473effe7b339460f
Dmitriy Fedotov <dmitriyfedotov@Dmitriys-MacBook-Pro.local>
2022-06-26 15:07:10 +0300

    added schedule to task

--- parser/app.py
+++ parser/app.py
@@ -1,4 +1,5 @@
 import requests
+import schedule
 
 from url_params import params, cookies, headers
 
@@ -72,7 +73,17 @@ class MvideoParser:
 
 
 parser = MvideoParser()
-# print(parser.get_product_ids())
-print(parser.get_json_data_and_params_from_products_ids())
-print(parser.add_model_info_to_tv_info())
-print(parser.add_prices_info_to_tv_info())
+
+
+def run_all_methods():
+    print(parser.get_json_data_and_params_from_products_ids())
+    print(parser.add_model_info_to_tv_info())
+    print(parser.add_prices_info_to_tv_info())
+
+
+schedule.every(30).seconds.do(run_all_methods)
+
+
+if __name__ == '__main__':
+    while True:
+        schedule.run_pending()
